3cea9feb8a91deef845b51a29f85f4074f8bb920
robotlinker <liangconghui@gmail.com>
2017-05-27 21:13:48 +0800

    revise the snap file to launch AWS and robot driver

--- aws_gateway/setup.py
+++ aws_gateway/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['aws_gateway'],
+    packages=[''],
     package_dir={'' : 'src'},
 )
 
